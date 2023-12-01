9bb558179a6fb5f328a9c6c7474adc21fa2e93a0
Fulcopin <fupifigu@espol.edu.ec>
2023-11-30 22:53:01 -0500

    cambio

--- src/main/java/com/mycompany/mavenproject11/Virtual_Care_Process.java
+++ src/main/java/com/mycompany/mavenproject11/Virtual_Care_Process.java
@@ -9,5 +9,43 @@ package com.mycompany.mavenproject11;
  * @author fulco
  */
 public class Virtual_Care_Process {
-    
+    public interface Decorador {
+    void enviarNotificacion();
+}
+
+public class DecoradorWire implements Decorador {
+    public void enviarNotificacion() {
+        // Implementación de la notificación para Wire
+    }
+}
+
+public class DecoradorSignal implements Decorador {
+    public void enviarNotificacion() {
+        // Implementación de la notificación para Signal
+    }
+}
+
+public class DecoradorWhatsapp implements Decorador {
+    public void enviarNotificacion() {
+        // Implementación de la notificación para Whatsapp
+    }
+}
+
+public class DecoradorTelegram implements Decorador {
+    public void enviarNotificacion() {
+        // Implementación de la notificación para Telegram
+    }
+}
+
+public class Cliente {
+    Decorador decorador;
+
+    public Cliente(Decorador decorador) {
+        this.decorador = decorador;
+    }
+
+    public void solicitarNotificacion() {
+        decorador.enviarNotificacion();
+    }
+}
 }
